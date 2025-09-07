b6aaf403d881aed771aad572074f677bc3fb2846
MohomedFarhan07 <218282897+MohomedFarhan07@users.noreply.github.com>
2025-09-07 13:18:01 +0530

    Small bug fix

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,7 @@
 const socket = require("socket.io");
 const crypto = require("crypto");
 const { Chat } = require("../models/chat");
+const connectionRequest = require("../models/connectionRequest");
 
 const getRoomId = (userId, targetUserId) => {
   return crypto
@@ -28,8 +29,27 @@ const initializeSocket = (server) => {
       async ({ firstName, lastName, text, userId, targetUserId }) => {
         try {
           const roomId = getRoomId(userId, targetUserId);
+
+          const isFriends = await connectionRequest.findOne({
+            $or: [
+              {
+                fromUserId: userId,
+                toUserId: targetUserId,
+                status: "accepted",
+              },
+              {
+                toUserId: targetUserId,
+                fromUserId: userId,
+                status: "accepted",
+              },
+            ],
+          });
+          if (!isFriends) {
+            return;
+          }
+
           let chat = await Chat.findOne({
-            participants: { $all: [userId, targetUserId]},
+            participants: { $all: [userId, targetUserId] },
           });
 
           if (!chat) {
